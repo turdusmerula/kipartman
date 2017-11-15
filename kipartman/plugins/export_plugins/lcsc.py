8bacb2d30f8e2dd436080b8635d7e83fc4982946
BSF-PC-AU1\BSFau <anthony@blacksheepfarm.co.nz>
2017-11-16 11:28:03 +1300

    [WIP] Commented out code in export extensions not yet integrated Have marked a #TODO: to implement as per comman csv

--- kipartman/plugins/export_plugins/lcsc.py
+++ kipartman/plugins/export_plugins/lcsc.py
@@ -8,23 +8,23 @@ class LCSCExport(_export_base.KiPartmanExporter):
 
     def export(self, base_filename, components):
         file_path = '{}.{}'.format(base_filename, self.extension)
+        #TODO: update this in line with changes made to csv_comma
+        # with open(file_path, 'w') as csvfile:
+        #     wrt = csv.writer(csvfile)
 
-        with open(file_path, 'w') as csvfile:
-            wrt = csv.writer(csvfile)
+        #     wrt.writerow(['Quantity','Value', 'Refs', 'Footprint',
+        #                 'Manufacturer','Manufacture Part Number',
+        #                 'LCSC Part Number'])
 
-            wrt.writerow(['Quantity','Value', 'Refs', 'Footprint',
-                        'Manufacturer','Manufacture Part Number',
-                        'LCSC Part Number'])
-
-            for fp in sorted(components):
-                for val in sorted(components[fp]):
-                    ctcont = components[fp][val]
-                    wrt.writerow([
-                        len(ctcont),
-                        ctcont.value,
-                        ctcont.refs,
-                        ctcont.footprint,
-                        ctcont.manufacturer,
-                        ctcont.manufacturer_pn,
-                        ctcont.supplier_pn,
-                    ])
+        #     for fp in sorted(components):
+        #         for val in sorted(components[fp]):
+        #             ctcont = components[fp][val]
+        #             wrt.writerow([
+        #                 len(ctcont),
+        #                 ctcont.value,
+        #                 ctcont.refs,
+        #                 ctcont.footprint,
+        #                 ctcont.manufacturer,
+        #                 ctcont.manufacturer_pn,
+        #                 ctcont.supplier_pn,
+        #             ])
--- kipartman/plugins/export_plugins/quoted_tab_csv.py
+++ kipartman/plugins/export_plugins/quoted_tab_csv.py
@@ -8,24 +8,25 @@ class QuotedTabExport(_export_base.KiPartmanExporter):
 
     def export(self, base_filename, components):
         file_path = '{}.{}'.format(base_filename, self.extension)
+        #TODO: update this in line with changes made to csv_comma
+ 
+        # with open(file_path, 'w') as csvfile:
+        #     wrt = csv.writer(csvfile, dialect='excel-tab')
 
-        with open(file_path, 'w') as csvfile:
-            wrt = csv.writer(csvfile, dialect='excel-tab')
+        #     wrt.writerow(['Refs', 'Value', 'Footprint',
+        #                   'Quantity', 'MFR', 'MPN', 'SPR', 'SPN'])
 
-            wrt.writerow(['Refs', 'Value', 'Footprint',
-                          'Quantity', 'MFR', 'MPN', 'SPR', 'SPN'])
-
-            for fp in sorted(components):
-                for val in sorted(components[fp]):
-                    ctcont = components[fp][val]
-                    commarefs = ctcont.refs.replace(';', ',')
-                    wrt.writerow([
-                        '"{}"'.format(commarefs),
-                        '"{}"'.format(ctcont.value),
-                        '"{}"'.format(ctcont.footprint),
-                        '"{}"'.format(len(ctcont)),
-                        '"{}"'.format(ctcont.manufacturer),
-                        '"{}"'.format(ctcont.manufacturer_pn),
-                        '"{}"'.format(ctcont.supplier),
-                        '"{}"'.format(ctcont.supplier_pn),
-                    ])
+        #     for fp in sorted(components):
+        #         for val in sorted(components[fp]):
+        #             ctcont = components[fp][val]
+        #             commarefs = ctcont.refs.replace(';', ',')
+        #             wrt.writerow([
+        #                 '"{}"'.format(commarefs),
+        #                 '"{}"'.format(ctcont.value),
+        #                 '"{}"'.format(ctcont.footprint),
+        #                 '"{}"'.format(len(ctcont)),
+        #                 '"{}"'.format(ctcont.manufacturer),
+        #                 '"{}"'.format(ctcont.manufacturer_pn),
+        #                 '"{}"'.format(ctcont.supplier),
+        #                 '"{}"'.format(ctcont.supplier_pn),
+        #             ])
