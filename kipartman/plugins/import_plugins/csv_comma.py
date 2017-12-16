614cfdc40fd64f2084c35a3f0030628a7d2cb531
BSF-PC-AU1\BSFau <anthony@blacksheepfarm.co.nz>
2017-12-16 15:25:37 +1300

    [WIP] Input CSV, mark todo on readers

--- kipartman/plugins/import_plugins/csv_comma.py
+++ kipartman/plugins/import_plugins/csv_comma.py
@@ -9,22 +9,22 @@ class CsvImport(_import_base.KiPartmanImporter):
     def fetch(self, base_filename, importItem = object):
         file_path = '{}.{}'.format(base_filename, self.extension)
 
-        with open(file_path, 'w') as csvfile:
-            wrt = csv.writer(csvfile)
+        with open(file_path, 'r') as csvfile:
+            rdr = csv.reader(csvfile)
+            #TODO: implement csvReader
+            # wrt.writerow(['Refs', 'Value', 'Footprint',
+            #               'Quantity', 'MFR', 'MPN', 'SPR', 'SPN'])
 
-            wrt.writerow(['Refs', 'Value', 'Footprint',
-                          'Quantity', 'MFR', 'MPN', 'SPR', 'SPN'])
-
-            for fp in sorted(components):
-                for val in sorted(components[fp]):
-                    ctcont = components[fp][val]
-                    wrt.writerow([
-                        ctcont.refs,
-                        ctcont.value,
-                        ctcont.footprint,
-                        len(ctcont),
-                        ctcont.manufacturer,
-                        ctcont.manufacturer_pn,
-                        ctcont.supplier,
-                        ctcont.supplier_pn,
-                    ])
+            # for fp in sorted(components):
+            #     for val in sorted(components[fp]):
+            #         ctcont = components[fp][val]
+            #         wrt.writerow([
+            #             ctcont.refs,
+            #             ctcont.value,
+            #             ctcont.footprint,
+            #             len(ctcont),
+            #             ctcont.manufacturer,
+            #             ctcont.manufacturer_pn,
+            #             ctcont.supplier,
+            #             ctcont.supplier_pn,
+            #         ])
--- kipartman/plugins/import_plugins/lcsc.py
+++ kipartman/plugins/import_plugins/lcsc.py
@@ -9,22 +9,22 @@ class LCSCImport(_import_base.KiPartmanImporter):
     def fetch(self, base_filename, importItem = object):
         file_path = '{}.{}'.format(base_filename, self.extension)
 
-        with open(file_path, 'w') as csvfile:
-            wrt = csv.writer(csvfile)
+        with open(file_path, 'r') as csvfile:
+            rdr = csv.reader(csvfile)
+            #TODO: Implement CSV reader
+            # wrt.writerow(['Quantity','Value', 'Refs', 'Footprint',
+            #             'Manufacturer','Manufacture Part Number',
+            #             'LCSC Part Number'])
 
-            wrt.writerow(['Quantity','Value', 'Refs', 'Footprint',
-                        'Manufacturer','Manufacture Part Number',
-                        'LCSC Part Number'])
-
-            for fp in sorted(components):
-                for val in sorted(components[fp]):
-                    ctcont = components[fp][val]
-                    wrt.writerow([
-                        len(ctcont),
-                        ctcont.value,
-                        ctcont.refs,
-                        ctcont.footprint,
-                        ctcont.manufacturer,
-                        ctcont.manufacturer_pn,
-                        ctcont.supplier_pn,
-                    ])
+            # for fp in sorted(components):
+            #     for val in sorted(components[fp]):
+            #         ctcont = components[fp][val]
+            #         wrt.writerow([
+            #             len(ctcont),
+            #             ctcont.value,
+            #             ctcont.refs,
+            #             ctcont.footprint,
+            #             ctcont.manufacturer,
+            #             ctcont.manufacturer_pn,
+            #             ctcont.supplier_pn,
+            #         ])
