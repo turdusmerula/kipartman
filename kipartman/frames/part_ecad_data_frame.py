e6b6bcd9393f28bbfc281be1f9747150ac08d0f6
seb <sebastien.besombes@gmail.com>
2017-12-17 19:28:35 +0100

    First shot for ECADdata panel

---
+++ kipartman/frames/part_ecad_data_frame.py
@@ -0,0 +1,7 @@
+from dialogs.panel_part_ecad_data import PanelPartEcadData
+import helper.tree
+
+
+class PartEcadDataFrame(PanelPartEcadData):
+    def __init__(self, parent): 
+        super(PartEcadDataFrame, self).__init__(parent)
