bbad284dc1716fb5c5e22506c6717cde7994f9e1
seb <sebastien.besombes@gmail.com>
2018-06-15 11:36:25 +0200

    Switched to www.ecb.europa.eu for currencies

--- kipartman/currency/currency.py
+++ kipartman/currency/currency.py
@@ -1,31 +1,36 @@
 import json
 import urllib
-
+import re
 # TODO: asynchronous load
 # TODO: store previous values in database
 class Currency(object):
-    def __init__(self, base):
-        self.base = base
-        self.currencies = []
+    def __init__(self):
+        self.base = 'EUR'
+        self.currencies = {}
         self.load()
         
     def load(self):
-        data = urllib.urlopen('http://api.fixer.io/latest?base='+self.base).read()
-        self.currencies = json.loads(data)
+        data = urllib.urlopen('http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml').read()
+        self.currencies = {}
+        for line in data.split('\n'):
+            if 'currency=' in line:
+                m = re.match(".*currency='(.*)' rate='(.*)'.*", line)
+                if m:
+                    currency = m.group(1)
+                    rate = float(m.group(2))
+                    self.currencies[currency] = rate
         print("Currencies: ", self.currencies)
         return self.currencies
         
     def convert(self, value, source, target):
-        rates = self.currencies['rates']
-        
         if source==self.base:
             rate_source_base = 1
         else:
-            rate_source_base = rates[source]
+            rate_source_base = self.currencies[source]
         if target==self.base:
             rate_source_target = 1
         else:
-            rate_source_target = rates[target]
+            rate_source_target = self.currencies[target]
         
         res = 0
         # convert source to base
